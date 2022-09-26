d33998e62ff3d3ebc07244e3700a0be84c0613c8
kaito <2516543531@qq.com>
2022-09-26 21:36:56 +0800

    周子豪first commit

--- ruoyi-system/src/main/java/com/ruoyi/contract/controller/TContractController.java
+++ ruoyi-system/src/main/java/com/ruoyi/contract/controller/TContractController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -58,6 +59,8 @@ public class TContractController extends BaseController
         ExcelUtil<TContract> util = new ExcelUtil<TContract>(TContract.class);
         util.exportExcel(response, list, "合同数据");
     }
+    
+
 
     /**
      * 获取合同详细信息
